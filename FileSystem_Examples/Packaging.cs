8cf7dd4969aa39fbb2ada914c2d0993033c7d9ad
jorge perez <jorgeperez00@hotmail.com>
2018-04-20 13:52:06 -0400

    adding packaging example

---
+++ FileSystem_Examples/Packaging.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSystem_Examples {
+    [TestClass]
+    public class Packaging {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
